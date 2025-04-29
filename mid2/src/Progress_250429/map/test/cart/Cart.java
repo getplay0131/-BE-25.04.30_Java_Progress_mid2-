1dc4fac2d7e24c69b07fdeb64652004324442634
seong_hyun_notebook <getplay0131@gmail.com>
2025-04-29 23:45:12 +0900

    25.04.29 2nd Check

---
+++ mid2/src/Progress_250429/map/test/cart/Cart.java
@@ -0,0 +1,14 @@
+package Progress_250429.map.test.cart;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cart {
+    private Map<Product, Integer> cartMap = new HashMap<>();
+
+    public void add(Product product, Integer integer){
+
+            }
+        }
+    }
+}
